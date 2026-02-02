c8d37251f68b1d4ee26cb78efa88001e45f4e142
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-02-02 09:08:21 +0500

    advance javascrip array methode

---
+++ script.js
@@ -0,0 +1,79 @@
+// let array=[1,2,3,4,5]
+// let neuArray=array.map((item)=>{
+//     return item *3;
+// })
+// console.log(neuArray);
+
+// const numbers = [12, 5, 8, 130, 44];
+
+// const bigNumbers = numbers.filter(num => num > 10);
+
+// console.log(bigNumbers);
+
+
+// const fruits = ["apple", "banana", "mango"];
+
+// fruits.forEach(fruit => {
+//   console.log("Fruit:", fruit);
+//   return fruits;
+// });
+
+// const users = [
+//     { name: "Ali", age: 12 },
+//     { name: "Sara", age: 18 },
+//     { name: "Ahmed", age: 25 }
+//   ];
+
+//   const name = users.map(user => user.name);
+
+// console.log(name);
+
+
+// const adults = users.filter(user => user.age >= 18);
+
+// console.log(adults);
+
+// const products = [
+//     { id: 1, name: "Laptop" },
+//     { id: 2, name: "Phone" },
+//     { id: 3, name: "Tablet" }
+//   ];
+  
+//   const product = products.find(item => item.id === 2);
+
+// console.log(product);
+
+// const scores = [70, 85, 90, 60];
+
+// const allPassed = scores.every(score => score > 50);
+
+// console.log(allPassed);
+
+
+// const emails = ["test@gmail.com", "hello@yahoo.com", "admin@gmail.com"];
+
+// const hasGmail = emails.some(email => email.includes("gmail"));
+
+// console.log(hasGmail);
+
+const students = [
+    { name: "Ali", marks: 80 },
+    { name: "Sara", marks: 45 },
+    { name: "Ahmed", marks: 90 },
+    { name: "Zara", marks: 60 }
+  ];
+
+  
+
+  const passedStudents = students.filter(student => student.marks >= 50).map(student => student.name);
+
+console.log(passedStudents);
+
+
+
+
+
+
+
+
+
